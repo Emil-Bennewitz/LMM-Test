78aa9f7ad93f3cd3bb159d7b4e3a89c10fe0aaa6
Almond-S <almond.stoecker@gmail.com>
2023-06-07 12:11:27 +0200

    small edits

--- snippets/build_R_package.R
+++ snippets/build_R_package.R
@@ -3,6 +3,7 @@
 devtools::create(".")
 
 # documentation using roxygen2 package -> standard in R studio
+# shortcut for building roxygen skeleton: Strf+Shift+Alt+R
 
 # run documentation
 devtools::document()
@@ -11,4 +12,6 @@ devtools::document()
 devtools::install()
 
 library(lme4randtest)
-?
+
+# for developers: load all functions into workspace
+devtools::load_all()
